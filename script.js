9175ef106bc1bf16ebcf2c5e8afad6117a738a59
Nick Pettis <pettisnick@gmail.com>
2020-06-28 18:08:50 -0400

    Functions

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-var generateBtn = document.querySelector("#generate");
+//Password variable values:
+var upperChoice = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChoice = "abcdefghijklmnopqrstuvwxyz";
+var numberChoice = "1234567890";
+var specialCharChoice = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-generateBtn.addEventListener("click", function () {
+//Choices declared outside the if statement so they can be concatenated upon condition
+var choices;
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
     ps = generatePassword();
     document.getElementById("password").placeholder = ps;
 });
 
-//Password variable values:
-var upperChoice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numberChoice = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharChoice = ["'", ".", ",", ":", ";", "=", "?", "/", "_", "~", "|", "&quot", "&apos", "&amp", "&lt", "&gt", "!", "$", "#", "%", "(", ")", "*", "[", "]", "+", "-"];
-
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-}
-//Start function to generate password
 function generatePassword() {
-    //Ask user for input
     var howManyChar = prompt("How many characters do you want the password to have? (Choose between 8 and 128 characters.");
-    if (howManyChar); {
+    if (!howManyChar) {
         alert("This needs a value");
+    
     } else if (howManyChar < 8 || howManyChar > 128) {
         howManyChar = parseInt(prompt("You must choose between 8 and 128!"));
+    
     } else {
         upper = confirm("Do you want to include uppercase letters in your password?");
 
@@ -32,17 +30,10 @@ function generatePassword() {
         number = confirm("Do you want to include numbers in your password?");
 
         specialChar = confirm("Do you want to include special characters in your password?");
-    };
-
-    if ()
-
-
-    }      
     
-
-    function newPass() {
-
-        if (upper === false && lower === false && number === false && specialChar === false) {
+    };
+        //4 negative options
+        if (!upper && !lower && !number && !specialChar) {
             alert("If you chose 'No' for everything, then you won't receive a new password");
         }
 
@@ -55,7 +46,7 @@ function generatePassword() {
         }
 
         while (password.length<howManyChar) {
-            else if (upper === true && lower === false && number === false && specialChar === false) {
+            else if (upper)   true && lower === false && number === false && specialChar === false) {
             for (var i = 0; i < parseInt(howManyChar); i++) {
                 randomNumber(upperChoice) //a single element from array
                 + randomNumber(lowerChoice)
